736f6074012698d9f0405c3cf72a5100fd185fae
jay <917647288@qq.com>
2012-10-16 15:03:20 +0800

    添加说明文件 和框架入口

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+// +----------------------------------------------------------------------
+// | Copyright (c) 2011 http://www.zjzit.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: Aoiujz <zuojiazi@vip.qq.com>
+// +----------------------------------------------------------------------
+// | index.php 2012-4-23
+// +----------------------------------------------------------------------
+
+define('APP_DEBUG', true);
+define('APP_NAME', 'Test');
+define('APP_PATH', './Test/');
+define('NOW_TIME', $_SERVER['REQUEST_TIME']);
+require_once './ThinkPHP/ThinkPHP.php';
